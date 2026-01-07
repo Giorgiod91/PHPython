f74e20c6b0b25a499466b0eb073f110fbd891306
Giorgiod91 <giorgio.dettmar@gmx.de>
2026-01-07 19:28:34 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+
+
+class its_PHP():
+
+    # i will create a function here to let a user output a php function with python code
+    def php_function(self, name:str, params:str, body=str):
+        
