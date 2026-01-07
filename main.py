97fd0bfb7f87aeec9033518e310093b81a1fd28c
Giorgiod91 <giorgio.dettmar@gmx.de>
2026-01-07 20:03:24 +0100

    adding a new approach where the full function will get converted

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-
+import inspect
 
 
 
 class its_PHP():
 
+
+   
+
     # i will create a function here to let a user output a php function with python code
-    def php_function(self, name:str, params:str, body=str):
+    def php_function(self, name:str, params:str, action_type=str ):
+        if(action_type == "print"):
+            reason = "echo" + ""        
+            the_function = f"<?php function {name}(${params}) {reason}  ?>"
+
+        return the_function
+    ## I will try another approach where the full function gets converted
+    def php_full(self, my_func):
+        source = inspect.getsource(my_func)
+        print(source)
+
+
+
+
+tryout = its_PHP()
+print(tryout.php_function("math_one", "x", "print"))
+
+def first_try():
+    x= "hello world"
+    print(x)
+    
+tryout.php_full(first_try)
+
+
+
         
