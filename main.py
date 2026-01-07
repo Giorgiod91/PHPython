6b888f3c3f6f6c7a164a8245ddee681a0c34e2e4
Giorgiod91 <giorgio.dettmar@gmx.de>
2026-01-07 20:08:58 +0100

    m,ore

--- main.py
+++ main.py
@@ -17,7 +17,16 @@ class its_PHP():
     ## I will try another approach where the full function gets converted
     def php_full(self, my_func):
         source = inspect.getsource(my_func)
-        print(source)
+        lines = source.splitlines()
+        new_array = []
+        for i in lines:
+            new_array.append(i)
+        if"print" in new_array:
+            new_array.remove("print")
+
+
+
+        print(new_array)
 
 
 
@@ -28,7 +37,7 @@ print(tryout.php_function("math_one", "x", "print"))
 def first_try():
     x= "hello world"
     print(x)
-    
+
 tryout.php_full(first_try)
 
 
